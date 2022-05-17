44b4d2956504f22fe0268222c6327e0d777b3096
Arfath Ahmed <arfathahmed19@gmail.com>
2022-05-17 14:01:58 +0530

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DisplaySectionComponent } from './display-section/display-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DisplaySectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-section/display-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-section',
+  templateUrl: './display-section.component.html',
+  styleUrls: ['./display-section.component.scss']
+})
+export class DisplaySectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
