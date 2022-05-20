97fdb81c499ccaf1954be3cb32799ad0464514c5
Arfath Ahmed <arfathahmed19@gmail.com>
2022-05-20 20:04:47 +0530

    Responsive for main section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,10 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (window.innerWidth < 610){
+      (document.querySelector('.items')as HTMLElement).style.display = 'none';
+      // (document.querySelector('.items_mobile')as HTMLElement).style.display = 'block';
+    }
   }
 
 }
