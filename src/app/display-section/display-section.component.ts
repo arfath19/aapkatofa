55782f0e4b9f2785239a321c1e4a236d45ef8ac2
Arfath Ahmed <arfathahmed19@gmail.com>
2022-05-24 13:32:28 +0530

    Added Jquery , Carousel responsive

--- src/app/display-section/display-section.component.ts
+++ src/app/display-section/display-section.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-display-section',
   templateUrl: './display-section.component.html',
   styleUrls: ['./display-section.component.scss']
 })
-export class DisplaySectionComponent implements OnInit {
+export class DisplaySectionComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    $("#myCarousel").on("slide.bs.carousel", function(e) {
+      var $e = $(e.relatedTarget);
+      var idx = $e.index();
+      var itemsPerSlide = 3;
+      var totalItems = $(".carousel-item").length;
+
+      if (idx >= totalItems - (itemsPerSlide - 1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i = 0; i < it; i++) {
+          // append slides to end
+          if (e.direction == "left") {
+            $(".carousel-item")
+              .eq(i)
+              .appendTo(".carousel-inner");
+          } else {
+            $(".carousel-item")
+              .eq(0)
+              .appendTo($(this).find(".carousel-inner"));
+          }
+        }
+      }
+    });
+  
   }
 
 }
