9cd3f9dee7ed8b8a91f37d5670f5ce348fb75e17
Arfath Ahmed <arfathahmed19@gmail.com>
2022-05-24 15:20:26 +0530

    Added 3 different carousel

--- src/app/display-section/display-section.component.ts
+++ src/app/display-section/display-section.component.ts
@@ -1,43 +1,16 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-declare var $: any;
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-display-section',
   templateUrl: './display-section.component.html',
   styleUrls: ['./display-section.component.scss']
 })
-export class DisplaySectionComponent implements OnInit, AfterViewInit {
+export class DisplaySectionComponent implements OnInit {
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-  }
-
-  ngAfterViewInit() {
-    $("#myCarousel").on("slide.bs.carousel", function(e) {
-      var $e = $(e.relatedTarget);
-      var idx = $e.index();
-      var itemsPerSlide = 3;
-      var totalItems = $(".carousel-item").length;
-
-      if (idx >= totalItems - (itemsPerSlide - 1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i = 0; i < it; i++) {
-          // append slides to end
-          if (e.direction == "left") {
-            $(".carousel-item")
-              .eq(i)
-              .appendTo(".carousel-inner");
-          } else {
-            $(".carousel-item")
-              .eq(0)
-              .appendTo($(this).find(".carousel-inner"));
-          }
-        }
-      }
-    });
-  
   }
 
 }
